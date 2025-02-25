1366852fd87779ed565900c7b10ec097091a6d84
NimishRathi <nimish.rathi2002@gmail.com>
2025-02-25 23:33:26 +0530

    deleted

--- UC1_EmployeePayroll/UC1_EmployeePayroll/src/main/java/com/capgemini/UC1_EmployeePayroll/Repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.UC1_EmployeePayroll.Repository;
-
-import com.capgemini.UC1_EmployeePayroll.Entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
--- UC1_EmployeePayroll/UC1_EmployeePayroll/src/main/java/com/capgemini/UC1_EmployeePayroll/exception2/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.capgemini.UC1_EmployeePayroll.exception2;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
